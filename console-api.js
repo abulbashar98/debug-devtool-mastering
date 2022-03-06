7002655fc2456eee3044020cacfe349e48a43fd7
abulbashar98 <abulbashar98116@gmail.com>
2022-03-06 17:30:50 +0600

    console-api

---
+++ console-api.js
@@ -0,0 +1,43 @@
+
+
+
+
+// Console Link: Shopping-cart-solution
+
+// 1. jquery selector, shortcut of query selector
+//    $('#phone-plus') === document.querySelector('#phone-plus');
+
+
+
+// 2. Common Logging levels.....
+
+//    console.info('normal-info');
+//     console.warn('Let me warn you about life....');
+//     console.error('user info does not match!!')
+
+// 3. Monitoring  a function.....
+//    monitor(updateProductNumber);
+
+//  4.  Stop monitoring a function.....
+//      unmonitor(updateProductNumber);
+
+// 5. Monitor events....
+//    const phonePlus = $('#phone-plus')
+//    monitorEvents(phonePlus);
+
+// 6. Monitor Particular events.....
+//    monitorEvents(phonePlus,['click','mouseout',mouseIn']);
+
+// 7. Unmonitor events......
+//    unmonitorEvents(phonePlus);
+
+
+// 8. Count function use  in vsCode, just like console.log()......
+//    console.count('function or object name', even function itself);
+
+// 9. Copy something to clipboard.....
+//   copy(anything)
+
+// 10. Get the event listers available in an object.....
+//      const phonePlus = $('#phone-plus')
+//       getEventListeners(phonePlus)
